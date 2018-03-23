cee0c473e8dbda376ffe78087097c0d9818e1d06
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-03-23 16:51:50 -0400

    Finished exercise

--- ContinueNodeJSServer/9JugglingAsync.js
+++ ContinueNodeJSServer/9JugglingAsync.js
@@ -28,3 +28,59 @@
     [after](https://npmjs.com/after). But for this exercise, try and do it
     without any external helper library. */
 
+var http = require('http');
+var urlNumber = 2;
+
+function getHttp(urlNumber) {
+    http.get(process.argv[urlNumber], function(response) {
+        var finalString = '';
+        response.setEncoding('utf8');
+        response.on('data', function(data) {
+            finalString += data;
+        });
+        response.on('end', function() {
+            console.log(finalString);
+            if(urlNumber < 4) {
+                getHttp(urlNumber + 1);
+            }
+        });
+    });
+}
+
+getHttp(2);
+
+/*  Here's the official solution in case you want to compare notes:
+
+    ─────────────────────────────────────────────────────────────────────────────
+
+        var http = require('http')
+        var bl = require('bl')
+        var results = []
+        var count = 0
+
+        function printResults () {
+            for (var i = 0; i < 3; i++) {
+                console.log(results[i])
+            }
+        }
+
+        function httpGet (index) {
+            http.get(process.argv[2 + index], function (response) {
+                response.pipe(bl(function (err, data) {
+                if (err) {
+                    return console.error(err)
+                }
+
+                results[index] = data.toString()
+                count++
+
+                if (count === 3) {
+                    printResults()
+                }
+                }))
+            })
+        }
+
+        for (var i = 0; i < 3; i++) {
+            httpGet(i)
+        } */

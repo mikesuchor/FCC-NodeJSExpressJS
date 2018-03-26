0de1cc231f64d669545b8c0b864fbc4390b40376
Michael Suchorolski <michael.suchorolski@bolivia.assuresoft.com>
2018-03-26 18:00:27 -0400

    Completed exercise

--- FinishNodeJSServer/12HttpUppercaser.js
+++ FinishNodeJSServer/12HttpUppercaser.js
@@ -47,3 +47,15 @@
 
     file://C:\Users\michael.suchorolski\AppData\Roaming\npm\node_modules\learnyounode\docs\through2-map.html */
 
+var http = require('http');
+var map = require('through2-map');
+
+var server = http.createServer(function(request, response) {
+    if(request.method === 'POST') {
+        request.pipe(map(function(chunk) {
+            return chunk.toString().toUpperCase();
+        })).pipe(response)
+    }
+});
+
+server.listen(process.argv[2]);
